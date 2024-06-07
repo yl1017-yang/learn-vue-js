3e725c9834b49dadde31a7ea0bb636fddf2cef2a
ylyu <yl.yu@pulmuone.com>
2024-06-07 22:00:34 +0900

    props

--- vue-cli-complete/src/App.vue
+++ vue-cli-complete/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+     <!-- 컴포넌트 파일명은 파스칼케이스 사용, 최소 2단어 사용 권장  ex) AppHeader -->
     <app-header 
       v-bind:propsdata="str"
       v-on:renew="renewStr"></app-header>
@@ -24,7 +25,7 @@ export default {
   // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
   data: function() {
     return {
-      str: 'Header'
+      str: 'Header!!!'  //props str 내보내기
     }
   },
   components: {
@@ -32,7 +33,7 @@ export default {
   },
   methods: {
     renewStr: function() {
-      this.str = 'hi';
+      this.str = 'hi';    //str 값을 바꾸는 emit 속성
     }
   }
 }
--- vue-cli-complete/src/components/AppHeader.vue
+++ vue-cli-complete/src/components/AppHeader.vue
@@ -1,16 +1,18 @@
 <template>
   <header>
     <h1>{{ propsdata }}</h1>
-    <button v-on:click="sendEvent">send</button>
+    <button v-on:click="sendEvent">send</button> <!-- App.vue에 emit 보내기 -->
   </header>
 </template>
 
 <script>
+import App from '../App.vue';
+
 export default {
   props: ['propsdata'],
   methods: {
     sendEvent: function() {
-      this.$emit('renew');
+      this.$emit('renew');  // renew 내용이 App.vue로 전달
     }
   }
 }
