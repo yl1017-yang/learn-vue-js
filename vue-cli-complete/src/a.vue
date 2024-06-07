e6c3275a3d69085cf24e1a295d7dafddd7a34934
ylyu <yl.yu@pulmuone.com>
2024-06-07 21:27:42 +0900

    a

---
+++ vue-cli-complete/src/a.vue
@@ -0,0 +1,29 @@
+<!-- 
+ var appHeader = { 
+  template: '<div>header</div>',
+  methods: {
+    addNum: function() {
+    }
+  }
+} 
+-->
+
+
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  // javascript - 인스턴스 옵션
+  methods: {
+    addNum: function() {
+      
+    }
+  }
+}
+</script>
+
+<style>
+/* css */
+</style>
